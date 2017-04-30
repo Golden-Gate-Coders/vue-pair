305c7d43933bc966ac5f59a1550484c439c563e7
Danish Mughal <mughal.danish@gmail.com>
2017-04-30 15:30:22 -0700

    Basic setup for weather app

---
+++ weather.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+  var app = new Vue({
+    el: '#weather',
+    data: {
+      location: ''
+    },
+    methods: {
+    }
+  });
+
+});
